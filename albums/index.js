0482e0ac03790aeca645b2de462a721100fcd9cf
Daniel Sanchez <neilsamur@gmail.com>
2018-05-25 15:09:09 +0200

    Adding code for first component

--- albums/index.js
+++ albums/index.js
@@ -1,4 +1,14 @@
+//Import library to help create a component
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Header from './src/components/header';
 
+// Create a component
+const App = () => {
+    return (
+      <Header headerText={'Albums'} />
+  );
+};
+
+// Render it to the device by registering at least one component
 AppRegistry.registerComponent('albums', () => App);
